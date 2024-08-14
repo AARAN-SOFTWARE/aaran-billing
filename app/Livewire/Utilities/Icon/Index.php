e02579e80a94a76da7b03ee1c2a5ba924285dc86
Divya <divyadharshini146@gmail.com>
2024-08-14 19:33:47 +0530

    #15 refactor icons.....

--- app/Livewire/Utilities/Icon/Index.php
+++ app/Livewire/Utilities/Icon/Index.php
@@ -8,14 +8,199 @@ use Livewire\Component;
 class Index extends Component
 {
 
-
     public function symbols()
     {
         return collect([
-            ['icon' => 'plus-slim'],
-            ['icon' => 'trash'],
-            ['icon' => 'save'],
-            ['icon' => 'pencil'],
+            'plus-slim',
+            'trash',
+            'save',
+            'pencil',
+            'chevrons-left-double',
+            'chevrons-left',
+            'chevrons-right',
+            'chevrons-down',
+            'chevrons-up',
+            'printer',
+            'cart',
+
+            'cog',
+            'arrow-circle-right',
+            'annotation',
+            'adjustments',
+            'refresh',
+            'check-circle',
+            'exclamation-circle',
+            'information-circle',
+            'x-circle',
+            'backward',
+            'clipboard-document',
+            'wifi',
+            'chat',
+            'share',
+
+            'login',
+            'log-out',
+            'power',
+            'clock',
+            'home',
+            'user',
+            'android',
+            'calculator',
+            'controller',
+            'database',
+            'dropbox',
+            'linkedin',
+            'skype',
+            'stack',
+            'twitter',
+
+            'twitter-x',
+            'unity',
+            'virus',
+            'youtube',
+            'zoom-in',
+            'zoom-out',
+            'wikipedia',
+            'dots-circle-horizontal',
+            'color-swatch',
+            'view-grid-add',
+            'scale',
+            'office-building',
+            'library',
+            'document-report',
+            'document-text',
+            'receipt-refund',
+            'list-bullet',
+            'document-duplicate',
+            'user-circle',
+            'notification',
+            'book-open',
+            'credit-card',
+
+            'clipboard-doc',
+            'folder',
+            'bulb',
+            'lock-closed',
+            'lock-open',
+            'minus',
+            'arrow-down',
+            'bar-center',
+            'window',
+            'truck',
+            'rocket',
+            'shop-bag',
+            'sparkles',
+            'users',
+            'user-group',
+            'dashboard-outline',
+            'mail',
+            'u-turn-left',
+
+            'u-turn-right',
+            'bug-ant',
+            'link',
+            'apple',
+            'cash',
+            'pen-nib',
+            'store',
+            'book',
+            'calender-1',
+            'grid',
+            'utilities',
+            'profile',
+            'cal-month',
+            'eye-slash',
+            'eye',
+            'trash-bold',
+            'upload',
+            'download',
+            'clip-board',
+            'thumbs-up',
+            'Check',
+            'x-mark',
+
+            'error',
+            'logo',
+            'message-round',
+            'login-key',
+            'perfomance',
+            'bag-hand',
+            'tablet-increase',
+            'contract-give',
+            'right-long-arrow',
+            'photo',
+            'phone',
+            'event',
+            'newspaper',
+            'arrow-right-circle',
+            'member-group',
+            'gift-top',
+            'chat1',
+            'chat2',
+            'logout',
+            'locate',
+            'square-left',
+
+            'square-left',
+            'angle-left',
+            'angle-right',
+            'add-image',
+            'search-alt',
+            'image-plus',
+            'image-user',
+            'image-circle',
+            'user-pen',
+            'crown'
+
+
+        ]);
+    }
+
+    public function fillIcon(){
+        return collect([
+            'twitter1',
+            'facebook-fill',
+            'check',
+            'git-hub',
+            'phone1',
+            'cmp',
+            'folder',
+            'folder1',
+            'fast-time',
+            'blinds',
+            'headset',
+            'msg-envelop',
+            'eclipse',
+            'square-left',
+            'square-right',
+            'angle-right',
+            'angle-left',
+            'paper-clip',
+
+            'dot-horizontal',
+            'dot-vertical',
+            'home',
+            'exclamation-circle',
+            'exclamation-triangle',
+            'plus-circle',
+            'info-circle',
+            'minus-circle',
+            'play-circle',
+            'pause-circle',
+            'question-circle',
+            'shield-check',
+            'x-circle',
+            'check-circle',
+            'back-space',
+            'photo',
+            'gif',
+
+            'gift',
+            'microphone',
+            'megaphone',
+            'pencil',
+
+
         ]);
     }
 
@@ -24,10 +209,15 @@ class Index extends Component
         return $this->symbols();
     }
 
+    public function getList1()
+    {
+        return $this->fillIcon();
+    }
+
     public function render()
     {
         return view('livewire.utilities.icon.index')->with([
-            'list' => $this->getList()
+            'list' => $this->getList(),'fill' => $this->getList1()
         ]);
     }
 }
--- resources/views/livewire/utilities/icon/index.blade.php
+++ resources/views/livewire/utilities/icon/index.blade.php
@@ -4,14 +4,24 @@
 
     <x-forms.m-panel>
 
-
-
         {{--<----row-1----->--}}
-        <div class="bg-white-900">
-            <div class="flex flex-row gap-2">
+        <div class="bg-white">
+            <div class="lg:grid lg:grid-cols-12 gap-2 md:grid md:grid-cols-8">
 
                 @foreach($list as $row)
-                <x-icons.utilities icon=" {{$row['icon'] }}"/>
+                    <x-icons.utilities :icon="$row"/>
+                @endforeach
+
+            </div>
+        </div>
+
+        <div class="border-b-2 border-amber-900">&nbsp;</div>
+
+        <div class="bg-white">
+            <div class="lg:grid lg:grid-cols-12 gap-2 md:grid md:grid-cols-8">
+
+                @foreach($fill as $row)
+                    <x-icons.utilities-fill :icon="$row" :colour="'#E90074'"/>
                 @endforeach
 
             </div>
