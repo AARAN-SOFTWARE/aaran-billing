b92abdfd935fce47477b52af6fc8ee93212716b4
Gowtham <gowtham@aaran.org>
2024-08-16 17:13:56 +0530

    #21 work on label

--- aaran/Common/Database/Seeders/S101_LabelSeeder.php
+++ aaran/Common/Database/Seeders/S101_LabelSeeder.php
@@ -5,14 +5,12 @@ namespace Aaran\Common\Database\Seeders;
 use Aaran\Common\Models\Common;
 use Illuminate\Database\Seeder;
 
-class S101_CommonSeeder extends Seeder
+class S101_LabelSeeder extends Seeder
 {
     public static function run(): void
     {
         Common::create([
             'vname' => '-',
-            'desc' => '-',
-            'desc_1' => '-',
             'active_id' => '1'
         ]);
     }
