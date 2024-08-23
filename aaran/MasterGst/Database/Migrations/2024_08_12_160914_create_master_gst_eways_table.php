391963202fb42dce1cc7b55fe63079c1db87c4ba
Gowtham <gowtham@aaran.org>
2024-08-23 19:41:18 +0530

    #52 work on sales gst

---
+++ aaran/MasterGst/Database/Migrations/2024_08_12_160914_create_master_gst_eways_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        if (Aaran\Aadmin\Src\Customise::hasGstApi()) {
+            Schema::create('master_gst_eways', function (Blueprint $table) {
+                $table->id();
+                $table->longText('ewbno');
+                $table->longText('ewbdt');
+                $table->longText('ewbvalidtill');
+                $table->timestamps();
+            });
+        }
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('master_gst_eways');
+    }
+};
