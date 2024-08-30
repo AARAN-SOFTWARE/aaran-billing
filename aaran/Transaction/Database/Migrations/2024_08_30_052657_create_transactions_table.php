2c71f0006992a0a68d726a5e538757490ac7e673
“SUNDAR-Office” <aaranoffice@gmail.com>
2024-08-30 12:01:15 +0530

    #71 working on transactions

--- aaran/Transaction/Database/Migrations/2024_08_30_052657_create_transactions_table.php
+++ aaran/Transaction/Database/Migrations/2024_08_30_052657_create_transactions_table.php
@@ -4,24 +4,41 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
 
     public function up(): void
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('contact_id')->references('id')->on('contacts');
             $table->string('acyear')->nullable();
-            $table->string('trans_type');
-            $table->string('mode');
+            $table->foreignId('company_id')->references('id')->on('companies');
+
+            $table->foreignId('contact_id')->references('id')->on('contacts');
+            $table->string('paid_to')->nullable();
+            $table->string('purpose')->nullable();
+            $table->foreignId('order_id')->references('id')->on('orders');
+
+            $table->foreignId('trans_type_id')->references('id')->on('commons');
+            $table->foreignId('mode_id')->references('id')->on('commons');
+
             $table->string('vdate');
-            $table->decimal('vname');
+            $table->decimal('amount', 15, 2);
+
             $table->foreignId('receipttype_id')->references('id')->on('commons');
+            $table->string('remarks');
             $table->string('chq_no')->nullable();
             $table->string('chq_date')->nullable();
-            $table->foreignId('bank_id')->references('id')->on('banks');
-            $table->string('remarks');
+            $table->foreignId('bank_id')->references('id')->on('commons');
+            $table->string('deposit_on')->nullable();
+            $table->string('realised_on')->nullable();
+
+            $table->foreignId('against_id')->references('id')->on('commons');
+            $table->string('ref_no')->nullable();
+            $table->string('ref_amount')->nullable();
+
+            $table->string('verified_by')->nullable();
+            $table->string('verified_no')->nullable();
+
             $table->timestamps();
         });
     }
