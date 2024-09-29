cb003a6b2fad89f2751d222d3536afc5e340cae0
SUNDAR <aaransoftware@gmail.com>
2024-09-29 21:59:46 +0530

    #140 refactor home slider

--- resources/views/components/slider/home-slider.blade.php
+++ resources/views/components/slider/home-slider.blade.php
@@ -1,7 +1,10 @@
 @props([
     'bg_image' => 'no image',
     'title'=>'Check This',
-    'slogan' => 'Check This'
+    'slogan' => 'Check This',
+    'title_text_colour'=> 'text-white',
+    'body_text_colour' => 'text-white',
+    'text_length' => 15
 ])
 
 <li {{ $attributes->class(['flex flex-col items-center justify-center w-full sm:h-screen h-full shrink-0 snap-start relative']) }}>
@@ -9,14 +12,15 @@
          class="w-full md:h-screen h-full sm:bg-cover bg-contain bg-no-repeat mx-auto flex-col brightness-75 flex justify-center relative">
     </div>
     <div
-            class="absolute bottom-1 left-10 w-auto h-10/12 flex-col text-white font-roboto p-5 my-5  px-10">
+        class="absolute bottom-8 left-10 w-auto h-10/12 flex-col font-roboto p-5 my-5  px-10">
         <div
-                class="sm:text-6xl sm:capitalize sm:drop-shadow-lg">
+            class="sm:text-6xl sm:capitalize sm:drop-shadow-lg {{$title_text_colour}}"
+        >
             {{$title}}
         </div>
         <div
-                class="sm:text-2xl text-xs mt-3 text-white">
-            {{\Illuminate\Support\Str::words($slogan,15)}}
+            class="sm:text-2xl text-xs mt-3  {{$body_text_colour}}">
+            {{\Illuminate\Support\Str::words($slogan,$text_length)}}
         </div>
 
     </div>
